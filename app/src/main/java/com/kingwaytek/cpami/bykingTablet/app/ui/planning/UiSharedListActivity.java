8417c9af7e8871e24454b593dd04bbb86fd0af2b
Vincent <vincent.chang@kingwaytek.com>
2016-09-02 19:19:56 +0800

    Keep working on data sharing functions.

---
+++ app/src/main/java/com/kingwaytek/cpami/bykingTablet/app/ui/planning/UiSharedListActivity.java
@@ -0,0 +1,7 @@
+package com.kingwaytek.cpami.bykingTablet.app.ui.planning;
+
+/**
+ * Created by vincent.chang on 2016/9/2.
+ */
+public class UiSharedListActivity {
+}
---
+++ app/src/main/java/com/kingwaytek/cpami/bykingTablet/utilities/adapter/SharedListAdapter.java
@@ -0,0 +1,7 @@
+package com.kingwaytek.cpami.bykingTablet.utilities.adapter;
+
+/**
+ * Created by vincent.chang on 2016/9/2.
+ */
+public class SharedListAdapter {
+}
